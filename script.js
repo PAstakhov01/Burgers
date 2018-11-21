85ddf0f23b76c94a0619edbf52fec4f577d0621a
Паша <pasha2001ads@mail.ru>
2018-11-21 21:02:09 +0300

    update 9.9.121221

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-const openButton = document.querySelector('hamburger-menu-link');
-const overlay = document.querySelector('overlay');
-const bars = document.querySelector('hamburger-menu-link__bars');
-const closeButton = document.querySelector('hamburger-menu-link');
-const menuLink = document.querySelectorAll('overlay__item a');
+var openButton = document.querySelector('.hamburger-menu-link');
+const overlay = document.querySelector('.overlay');
+const bars = document.querySelector('.hamburger-menu-link__bars');
+const closeButton = document.querySelector('.hamburger-menu-link');
+const menuLink = document.querySelectorAll('.overlay__item a');
 
-openButton.addEventListener( 'click', function (a) {
+openButton.addEventListener( 'click', function (e) {
    e.preventDefault();
 
-   bars.classList.toggle(hamburger-menu-link);
-   if (overlay.style.display === flex) {
-       overlay.style.display = none;
+   bars.classList.toggle('.hamburger-menu-link');
+   if (overlay.style.display === 'flex') {
+       overlay.style.display = 'none';
    } else {
-       overlay.style.display = flex;
+       overlay.style.display = 'flex';
    };
 });
 
 for (var i = 0; i < menuLink.length; i++) {
-   menuLink[i].addEventListener(click, function () {
-       bars.classList.toggle(hamburger-menu-link);
-       if (overlay.style.display === flex) {
-           overlay.style.display = none;
+   menuLink[i].addEventListener('click', function () {
+       bars.classList.toggle('.hamburger-menu-link');
+       if (overlay.style.display === 'flex') {
+           overlay.style.display = 'none';
        } else {
-           overlay.style.display = flex;
+           overlay.style.display = 'flex';
        };
    });
 }
